aa70a2f8075654db8d8c3aa72528d3da8ea7746d
Shripad K <assortmentofsorts@gmail.com>
2010-10-27 15:33:37 +0530

    Extracted code from connection.js,bare-bones low level api similar to socket.io! Yay!

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = module.exports = express.createServer();
 
 // Configure your Redis client here!
 
-configure();
+configureClient();
 
 // Configuration
 
@@ -53,9 +53,55 @@ app.post('/', function(req, res) {
 })
 
 // Only listen on $ node app.js
-
 if (!module.parent) {
   app.listen(3000);
   console.log("Express server listening on port %d", app.address().port)
 	Juggernaut.listen(app);
 }
+
+
+	
+	Juggernaut.Connection.include({
+	  init: function(stream){
+	    this.stream     = stream;
+	    this.session_id = this.stream.sessionId;
+	    this.client     = new Client(this);
+
+	    this.stream.on("message", this.proxy(this.onmessage));
+	    this.stream.on("disconnect", this.proxy(this.ondisconnect));
+	  },
+	  onmessage: function(data){
+	    console.log("Received: " + data);
+
+	    try {
+	      var message = Message.fromJSON(data);
+	    } catch(e) { return; }
+
+	    switch (message.type){
+	      case "subscribe":
+	        this.client.subscribe(message.getChannel());
+	      break;
+	      case "unsubscribe":
+	        this.client.unsubscribe(message.getChannel());
+	      break;
+	      case "meta":
+	        this.client.setMeta(message.data);
+	      break;
+	      case "event":
+	        this.client.event(message.data);
+	      break;
+	      default:
+	        throw "Unknown type"
+	    }
+	  },
+
+	  ondisconnect: function(){
+	    this.client.disconnect();
+	  },
+
+	  write: function(message){
+	    if (typeof message.toJSON == "function")
+	      message = message.toJSON();
+	    this.stream.send(message);
+	  }
+	});
